baaf97a14093767f9a5cffbeed8320e02d89347d
Matvey Egorov <61591322+mvoroge@users.noreply.github.com>
2026-05-08 22:08:58 +0300

    fix MAX_CONNECTIONS

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ ulimit -n 4096
 
 SECRET=${SECRET:-0123456789abcdef0123456789abcdef}
 PORT=${PORT:-443}
-MAX_CONNECTIONS=${PORT:-128}
+MAX_CONNECTIONS=${MAX_CONNECTIONS:-128}
 
 exec /opt/MTProxy/objs/bin/mtproto-proxy \
   -u nobody \
