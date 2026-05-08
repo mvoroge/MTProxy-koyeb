192aa1f64e42e01d01d6923e3666dd046fe39f8c
Matvey Egorov <61591322+mvoroge@users.noreply.github.com>
2026-05-08 21:53:41 +0300

    add max connections

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ ulimit -n 4096
 
 SECRET=${SECRET:-0123456789abcdef0123456789abcdef}
 PORT=${PORT:-443}
+MAX_CONNECTIONS=${PORT:-128}
 
 exec /opt/MTProxy/objs/bin/mtproto-proxy \
   -u nobody \
@@ -11,6 +12,7 @@ exec /opt/MTProxy/objs/bin/mtproto-proxy \
   -p 8888 \
   -S "${SECRET}" \
   -C 256 \
+  -c "${MAX_CONNECTIONS}" \
   --aes-pwd /opt/MTProxy/proxy-secret /opt/MTProxy/proxy-multi.conf \
   --http-stats \
   -M 1
